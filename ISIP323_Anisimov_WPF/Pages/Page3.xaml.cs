11c8d04ae25b7b13f01d4772bdeba0a791c8c290
234918 <234918@KB37-303-C5>
2026-01-29 16:02:52 +0300

    Доделывай ещё, почти пофиксил вывод

--- ISIP323_Anisimov_WPF/Pages/Page3.xaml.cs
+++ ISIP323_Anisimov_WPF/Pages/Page3.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,9 +25,10 @@ namespace ISIP323_Anisimov_WPF.Pages
         string Name, Email, Address;
         public Page3(List<Product> basket)
         {
-        Basket = basket;
-                    InitializeComponent();
-                }
+            Basket = basket;
+            InitializeComponent();
+            TextZakaz();
+        }
         private void EmailUser_TextChanged(object sender, TextChangedEventArgs e)
         {
             Email = EmailUser.Text;
@@ -37,22 +39,47 @@ namespace ISIP323_Anisimov_WPF.Pages
             Address = AddresUser.Text;
         }
 
-       
-        
+
+
         private void NameUser_TextChanged(object sender, TextChangedEventArgs e)
         {
             Name = NameUser.Text;
         }
-       
+        int countProducts1 = 0, countProducts2 = 0, countProducts3 = 0;
+        bool iter1 = true, iter2 = true, iter3 = true;
         void TextZakaz()
         {
-            string zakaz = "";
+            string zakaz = $"";
             foreach (var item in Basket)
             {
-                
-            }
-            ZakazTexbBlock.Text = "Вы заказали" + zakaz;
-        }
+                if (item.ID == 1) 
+                { 
+                    countProducts1++;
+                    if (iter1) { Product1.Content = $"{item.Name} за {item.Price} "; iter1 = false; }
+                }
+                else if (item.ID == 2) 
+                { 
+                    countProducts2++;
+                    if (iter2) { Product2.Content = $"{item.Name} за {item.Price} "; iter2 = false; }
+                }
+                else if (item.ID == 3) 
+                { 
+                    countProducts3++;
+                    if (iter3) { Product1.Content = $"{item.Name} за {item.Price} "; iter3 = false; }
+                }
 
+            }
+            Product1.Content += $"{countProducts1}";
+            Product2.Content += $"{countProducts2}";
+            Product3.Content += $"{countProducts3}";
+
+
+            if (countProducts1 < 0) { Product1.Visibility = Visibility.Hidden; }
+            if (countProducts2 < 0) { Product1.Visibility = Visibility.Hidden; }
+            if (countProducts3 < 0) { Product1.Visibility = Visibility.Hidden; }
+
+
+        }
     }
 }
+
